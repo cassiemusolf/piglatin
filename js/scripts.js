74994bd6345786dd0bf9f6a2dba738fb5b4f58b6
Brandon Cooper and Cassie Musolf <brandon@>
2017-01-18 11:32:45 -0800

    start pig latin

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var translator = function(input) {
+  var array = input.split('');
+  var latin = "ay";
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  array.push(latin);
+
+  for (var i = 0; i < vowels.length ; i++) {
+    if ()
+  }
+}
+
+
+
+
+
+
+$(function () {
+
+var userInput = $('input#pig').val();
+
+
+
+
+  $('form').submit(function() {
+  var userInput = $('input#pig').val();
+  $('.display').text(array);
+  event.preventDefault();
+  });
+});
