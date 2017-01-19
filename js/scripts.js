da19984872e4b66c5e6e0e59ed379bbc4363780d
Brandon Cooper and Cassie Musolf <cassie@>
2017-01-18 16:28:06 -0800

    add function for qu input

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 var latin = "ay";
 var vowels = ['a', 'e', 'i', 'o', 'u'];
 
-
 var translator = function(userInput) {
-  for (var i = 0; i < userInput.length ; i++) {
+  for (var i = 0; i < vowels.length ; i++) {
     if (vowels.includes(userInput.charAt(0))) {
       return userInput += latin;
-    } else if ((vowels.indexOf(userInput[0] === !vowels)) && (vowels.indexOf(userInput[1] === !vowels))) {
+    } else if ((vowels.indexOf(userInput[0] === -1)) && (vowels.indexOf(userInput[1] === -1))) {
       return userInput.slice(2, userInput.length) + userInput.slice(0,2) + "ay";
-    } else if (vowels.indexOf(userInput[0] === !vowels)) {
+    } else if (vowels.indexOf(userInput[0] === -1)) {
       return userInput.slice(1, userInput.length) + userInput.slice(0,1) + "ay";
     } else {
         return false;
+      }
+    }
+  }
+
+var qu = function(userInput) {
+  for (var i = 0; i < userInput.length ; i++) {
+    if (userInput.match("qu")) {
+      return userInput.slice(2, userInput.length) + userInput.slice(0,2) + "ay";
+  } else {
+    return false;
       }
     }
   }
